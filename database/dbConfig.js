41781c97675d1d55cd1a7e49528a8153ff30763f
Kamran Memon <kamranmemon25@gmail.com>
2020-08-06 12:39:13 +0530

    Database Config Problem Solved

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,15 +1,16 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
 
 let sqlConfig = {
   mysql_pool: mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     multipleStatements: true,
   }),
 };
-
-console.log("Database Connected");
 
 module.exports = sqlConfig;
