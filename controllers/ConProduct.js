ef4b39874b5ba2f87698332baf85504e83463aef
Kamran Memon <kamranmemon25@gmail.com>
2020-08-14 14:54:13 +0530

    simple Api post and Get Data from admin-frontend is done

--- controllers/ConProduct.js
+++ controllers/ConProduct.js
@@ -3,27 +3,38 @@ import sqlConfig from "../database/dbConfig";
 let sql = sqlConfig.mysql_pool;
 
 exports.add = (req, res) => {
-  const { name, shortDescription, price, discount } = req.body;
+  const {
+    name,
+    shortDescription,
+    specification,
+    price,
+    discount,
+    finalPrice,
+  } = req.body;
 
   let addQuery =
-    "INSERT INTO products(name,short_description,price,discount) VALUES('" +
+    "INSERT INTO products(name,short_description,specification,price,discount,final_price) VALUES('" +
     name +
     "','" +
     shortDescription +
     "','" +
+    specification +
+    "','" +
     price +
     "','" +
     discount +
+    "','" +
+    finalPrice +
     "');";
 
   sql.query(addQuery, (err, result) => {
     if (err) throw err;
-    console.log(result);
-    res.json("product uploaded");
+    console.log("product Uploaded");
+    res.json("Product uploaded");
   });
 };
 
-exports.get = (req, res) => {
+exports.show = (req, res) => {
   let showProducts = "SELECT * from products";
   sql.query(showProducts, (err, result) => {
     if (err) throw err;
--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,7 @@ import conProduct from "../controllers/ConProduct";
 
 //Defining functions as per Routes
 router.post("/add", conProduct.add); //Showing all the products
-router.get("/get", conProduct.get); //Showing all the products
+router.get("/show", conProduct.show); //Showing all the products
 
 //Exporting Router
 module.exports = router;
